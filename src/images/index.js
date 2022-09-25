a30cc600de168a5424eaec09e80562f5a16f1da5
Prasanna T <thapap@oregonstate.edu>
2022-09-25 10:02:02 -0400

    refactored

--- src/images/index.js
+++
@@ -1,4 +0,0 @@
-export const chess = require("./chess.svg");
-export const inamorata = require("./cocktail.svg");
-export const tipbot = require("./robot.svg");
-export const portfolio = require("./internet.svg");
